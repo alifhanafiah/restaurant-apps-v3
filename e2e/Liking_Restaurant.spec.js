b4d01a967368362e57f225f690f24ee950b41fc6
alif-hanafiah <muhalifhanafiah@gmail.com>
2022-11-30 23:35:18 +0700

    menambahkan e2e testing like restaurant

---
+++ e2e/Liking_Restaurant.spec.js
@@ -0,0 +1,73 @@
+const assert = require('assert');
+
+Feature('Liking Restaurants');
+
+Before(({ I }) => {
+  I.amOnPage('/#/favorite');
+});
+
+Scenario('showing empty liked restaurants', ({ I }) => {
+  I.seeElement('#query');
+  // I.seeElement('.query'); // membuat test menjadi gagal
+  I.see('Tidak ada restaurant untuk ditampilkan', '.restaurant-item__not__found');
+});
+
+Scenario('liking one restaurant', async ({ I }) => {
+  I.see('Tidak ada restaurant untuk ditampilkan', '.restaurant-item__not__found');
+
+  I.amOnPage('/');
+
+  I.seeElement('.card__body__name a');
+
+  const firstRestaurant = locate('.card__body__name a').first();
+  const firstRestaurantName = await I.grabTextFrom(firstRestaurant);
+  I.click(firstRestaurant);
+
+  I.seeElement('#likeButton');
+  I.click('#likeButton');
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('.restaurant__item');
+  const likedRestaurantName = await I.grabTextFrom('.card__body__name');
+
+  assert.strictEqual(firstRestaurantName, likedRestaurantName);
+});
+
+Scenario('searching restaurants', async ({ I }) => {
+  I.see('Tidak ada restaurant untuk ditampilkan', '.restaurant-item__not__found');
+
+  I.amOnPage('/');
+
+  I.seeElement('.card__body__name a');
+
+  const names = [];
+
+  for (let i = 1; i <= 3; i++) {
+    I.click(locate('.card__body__name a').at(i));
+
+    I.seeElement('#likeButton');
+
+    I.click('#likeButton');
+
+    names.push(await I.grabTextFrom('.detail__title'));
+
+    I.amOnPage('/');
+  }
+
+  I.amOnPage('/#/favorite');
+  I.seeElement('#query');
+
+  const searchQuery = names[1].substring(1, 3);
+  const matchingRestaurants = names.filter((name) => name.indexOf(searchQuery) !== -1);
+
+  I.fillField('#query', searchQuery);
+  I.pressKey('Enter');
+
+  const visibleFavoritedRestaurants = await I.grabNumberOfVisibleElements('.restaurant__item');
+  assert.strictEqual(matchingRestaurants.length, visibleFavoritedRestaurants);
+
+  matchingRestaurants.forEach(async (name, index) => {
+    const visibleName = await I.grabTextFrom(locate('.card__body__name').at(index + 1));
+    assert.strictEqual(name, visibleName);
+  });
+});
--- karma.conf.js
+++ karma.conf.js
@@ -1,5 +1,6 @@
 // Karma configuration
 // Generated on Fri Jul 03 2020 20:15:52 GMT+0700 (Western Indonesia Time)
+// eslint-disable-next-line func-names
 module.exports = function (config) {
   config.set({
     // base path that will be used to resolve all patterns (eg. files, exclude)
